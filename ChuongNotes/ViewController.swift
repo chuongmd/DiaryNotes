33ce0ba5a399c8726cac4b1d2426a1b19c550008
ChuongMD <chuongmd91@gmail.com>
2019-01-24 10:27:25 +0700

    Initial commit

---
+++ ChuongNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuongNotes
+//
+//  Created by Mai Dinh Chuong on 1/23/19.
+//  Copyright © 2019 Mai Dinh Chuong. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController
+{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
